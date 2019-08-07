ea525e2219824269bb2fa46bb26aaeb78e45b823
Janice-M <janiceink001@gmail.com>
2019-08-07 11:04:23 +0300

    initialized everything we need in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 #flask extension instances
 
@@ -12,12 +13,31 @@ login_manager.session_protection = 'strong'
 bootstrap = Bootstrap ()
 db = SQLAlchemy()
 mail= Mail()
+simple = SimpleMDE()
 
-#initializing a flask application 
 
 def create_app(config_name)
 
 
     app = Flask(__name__)
+    simple.init_app(app)
 
-    from app import views
+
+
+    app.config.from_object(config_options[config_name])
+    
+    
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    
+    
