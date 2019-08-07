21ab93ceb88df24637aa9ce20ed41de04f49e516
Janice-M <janiceink001@gmail.com>
2019-08-07 16:56:15 +0300

    added main/forms.py && radio buttons

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,7 +5,7 @@ from wtforms import ValidationError
 
 
 
-class PitchForm(FlaskForm):
+class PizzaForm(FlaskForm):
 	title = StringField('Title', validators=[Required()])
 	description = TextAreaField("Rafiki, Select a Pizza of your choice",validators=[Required()])
 	category = RadioField('Label', choices=[ ('extra large ','extra large'), ('large pizza','large pizza'),('medium pizza','medium pizza'),('small pizza','small pizza')],validators=[Required()])
